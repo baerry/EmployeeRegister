50f3d92f141435278d3b4298b57777b5b16eeb27
tobias sjöblom <tobiasdev12@hotmail.com>
2017-05-24 20:41:45 +0200

    Init Add
    read title

---
+++ DataAccessLayer/EmployeesContex.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Security.AccessControl;
+
+namespace EmployeeRegister.DataAccessLayer
+{
+    public class EmployeesContex : DbContext
+    {
+
+        public DbSet<Models.Employee> Employees { get; set; }
+        public EmployeesContex() : base("name=DefaultConnection")
+        {
+            
+        }
+           
+    }
+
+        
+    
+
+
+}
+
---
+++ Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeRegister.Models
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public int Salery { get; set; }
+        public string Department { get; set; }
+        public string Position { get; set; }
+    }
+}
